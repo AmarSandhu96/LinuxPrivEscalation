1cd67fba0f2e8241aedadf5e72ee33efde757a05
Arch <arch>
2023-11-17 21:11:03 +0000

    LinuxPriv ~ Github Actions Edit 12

--- tests/CompilationTest.py
+++ tests/CompilationTest.py
@@ -1,13 +1,17 @@
 import os
 
+
 os.system("rm bin/privesc.elf")
+
 print("[+] Removed privesc.elf")
 print("[+] Compiling Project")
 
 os.system("g++ -o bin/privesc.elf src/privesc.cpp -g -static")
 if os.path.exists("bin/privesc.elf"):
     print("[+] Compilation Successful")
+    exit (0)
 else:
     print("[!] Compilication Failed")
+    exit (1)
 
 
